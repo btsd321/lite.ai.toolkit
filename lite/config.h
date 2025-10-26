95808231a9855e3c84fdea4ec2d8c385ad74d305
btsd321 <1123347532@qq.com>
2025-10-26 21:04:52 +0800

    启用 TensorRT 支持并在安装阶段安装 libddim_scheduler_cpp.so
    在 lite/config.h 中定义 ENABLE_TENSORRT 以开启 TensorRT 支持。
    在 CMakeLists.txt 中新增检测并安装 lite/bin/libddim_scheduler_cpp.so 的逻辑（若存在则安装到 lib 并输出提示）。

--- lite/config.h
+++ lite/config.h
@@ -2,7 +2,7 @@
 #define LITE_AI_CONFIG_H
 
 #define ENABLE_ONNXRUNTIME
-/* #undef ENABLE_TENSORRT */
+#define ENABLE_TENSORRT
 /* #undef ENABLE_MNN */
 /* #undef ENABLE_NCNN */
 /* #undef ENABLE_TNN */
